e8276526c36a6036dd1fa1aa10954387a64ed097
Taulant Ibraj <tibraj94@gmail.com>
2020-04-02 15:27:07 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,7 +5,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        #your hashes here!
+        "Benvolio" => {:name => "Benvolio", :age => "17", :attitude => "worried"}
+        "Mercutio" => {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}  
       ]
    }, 
    :capulet => {
@@ -13,7 +14,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+        "Steven" => {:name => "Steven", :age => "30", :attitude => "confused"}
+        "Nurse" => {:name => "Nurse", :age => "44", :attitude => "worried"}
       ]
    }
 }
